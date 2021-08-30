6e4b17801a345f436e133b7dd659c56beb13d77d
MeiyuanXiang <meiguixiangmanyuan@163.com>
2021-08-30 11:42:11 +0800

    第四节，实现前端里程计初始

--- src/lidar_localization/src/front_end_node.cpp
+++ src/lidar_localization/src/front_end_node.cpp
@@ -1,8 +1,8 @@
 /*
- * @Description: 基本框架测试程序
+ * @Description: 前端里程计初试测试程序
  * @Author: Xiangmy
  * @Date: 2021-08-27
- * @Run: roslaunch lidar_localization test_frame.launch
+ * @Run: roslaunch lidar_localization front_end.launch
  */
 
 #include <ros/ros.h>
@@ -16,6 +16,7 @@
 #include "lidar_localization/tf_listener/tf_listener.hpp"
 #include "lidar_localization/publisher/cloud_publisher.hpp"
 #include "lidar_localization/publisher/odometry_publisher.hpp"
+#include "lidar_localization/front_end/front_end.hpp"
 
 using namespace lidar_localization;
 
@@ -25,7 +26,7 @@ int main(int argc, char *argv[])
     FLAGS_log_dir = WORK_SPACE_PATH + "/Log";
     FLAGS_alsologtostderr = 1;
 
-    ros::init(argc, argv, "test_frame_node");
+    ros::init(argc, argv, "front_end_node");
     ros::NodeHandle nh;
 
     // 话题订阅
@@ -36,7 +37,12 @@ int main(int argc, char *argv[])
 
     // 话题发布
     std::shared_ptr<CloudPublisher> cloud_pub_ptr = std::make_shared<CloudPublisher>(nh, "current_scan", 100, "/map");
-    std::shared_ptr<OdometryPublisher> odom_pub_ptr = std::make_shared<OdometryPublisher>(nh, "lidar_odom", "map", "lidar", 100);
+    std::shared_ptr<CloudPublisher> local_map_pub_ptr = std::make_shared<CloudPublisher>(nh, "local_map", 100, "/map");
+    std::shared_ptr<CloudPublisher> global_map_pub_ptr = std::make_shared<CloudPublisher>(nh, "global_map", 100, "/map");
+    std::shared_ptr<OdometryPublisher> laser_odom_pub_ptr = std::make_shared<OdometryPublisher>(nh, "laser_odom", "map", "lidar", 100);
+    std::shared_ptr<OdometryPublisher> gnss_pub_ptr = std::make_shared<OdometryPublisher>(nh, "gnss", "map", "lidar", 100);
+
+    std::shared_ptr<FrontEnd> front_end_ptr = std::make_shared<FrontEnd>();
 
     std::deque<CloudData> cloud_data_buff;
     std::deque<IMUData> imu_data_buff;
@@ -44,6 +50,15 @@ int main(int argc, char *argv[])
     Eigen::Matrix4f lidar_to_imu = Eigen::Matrix4f::Identity();
     bool transform_received = false;
     bool gnss_origin_position_inited = false;
+    bool front_end_pose_inited = false;
+    CloudData::CLOUD_PTR local_map_ptr(new CloudData::CLOUD());
+    CloudData::CLOUD_PTR global_map_ptr(new CloudData::CLOUD());
+    CloudData::CLOUD_PTR current_scan_ptr(new CloudData::CLOUD());
+
+    double run_time = 0.0;
+    double init_time = 0.0;
+    bool time_inited = false;
+    bool has_global_map_published = false;
 
     ros::Rate rate(100);
     while (ros::ok())
@@ -60,7 +75,6 @@ int main(int argc, char *argv[])
             if (lidar_to_imu_ptr->LookupData(lidar_to_imu))
             {
                 transform_received = true;
-                //LOG(INFO) << "lidar to imu transform matrix is:" << std::endl << lidar_to_imu;
             }
         }
         else
@@ -70,6 +84,16 @@ int main(int argc, char *argv[])
                 CloudData cloud_data = cloud_data_buff.front();
                 IMUData imu_data = imu_data_buff.front();
                 GNSSData gnss_data = gnss_data_buff.front();
+
+                if (!time_inited)
+                {
+                    time_inited = true;
+                    init_time = cloud_data.time;
+                }
+                else
+                {
+                    run_time = cloud_data.time - init_time;
+                }
 
                 double d_time = cloud_data.time - imu_data.time;
                 if (d_time < -0.05)
@@ -87,7 +111,7 @@ int main(int argc, char *argv[])
                     imu_data_buff.pop_front();
                     gnss_data_buff.pop_front();
 
-                    Eigen::Matrix4f odometry_matrix;
+                    Eigen::Matrix4f odometry_matrix = Eigen::Matrix4f::Identity();
 
                     if (!gnss_origin_position_inited)
                     {
@@ -95,18 +119,37 @@ int main(int argc, char *argv[])
                         gnss_origin_position_inited = true;
                     }
 
-                    // 将点云坐标转换为地理坐标，并转换到IMU坐标系
                     gnss_data.UpdateXYZ();
                     odometry_matrix(0, 3) = gnss_data.local_E;
                     odometry_matrix(1, 3) = gnss_data.local_N;
                     odometry_matrix(2, 3) = gnss_data.local_U;
                     odometry_matrix.block<3, 3>(0, 0) = imu_data.GetOrientationMatrix();
                     odometry_matrix *= lidar_to_imu;
+                    gnss_pub_ptr->Publish(odometry_matrix);
 
-                    pcl::transformPointCloud(*cloud_data.cloud_ptr, *cloud_data.cloud_ptr, odometry_matrix);
+                    if (!front_end_pose_inited)
+                    {
+                        front_end_pose_inited = true;
+                        front_end_ptr->SetInitPose(odometry_matrix);
+                    }
 
-                    cloud_pub_ptr->Publish(cloud_data.cloud_ptr);
-                    odom_pub_ptr->Publish(odometry_matrix);
+                    front_end_ptr->SetPredictPose(odometry_matrix);
+                    Eigen::Matrix4f laser_matrix = front_end_ptr->Update(cloud_data);
+                    laser_odom_pub_ptr->Publish(laser_matrix);
+
+                    front_end_ptr->GetCurrentScan(current_scan_ptr);
+                    cloud_pub_ptr->Publish(current_scan_ptr);
+                    if (front_end_ptr->GetNewLocalMap(local_map_ptr))
+                        local_map_pub_ptr->Publish(local_map_ptr);
+                }
+
+                if (run_time > 460.0 && !has_global_map_published)
+                {
+                    if (front_end_ptr->GetNewGlobalMap(global_map_ptr))
+                    {
+                        global_map_pub_ptr->Publish(global_map_ptr);
+                        has_global_map_published = true;
+                    }
                 }
             }
         }
